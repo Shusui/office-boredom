98d4d78e49a39216e701b6424b6f3ed972bff48f
David Gomes <davidrafagomes@gmail.com>
2014-01-25 08:38:50 +0000

    Fixed code style.

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,60 +1,60 @@
 #include "Player.hpp"
 
 Player::Player() {
-	x = 0;
-	y = 0;
-	speed = 3;
+  x = 0;
+  y = 0;
+  speed = 3;
 
-	buffer.loadFromFile("res/satisfactionSound.wav");
-	satisfactionSound.setBuffer(buffer);
-	satisfactionSound.setLoop(true);
-  
-	texture.loadFromFile("res/player.png");
-	sprite.setTexture(texture);
+  buffer.loadFromFile("res/satisfactionSound.wav");
+  satisfactionSound.setBuffer(buffer);
+  satisfactionSound.setLoop(true);
 
-	satisfaction = 0;
-	satisfactionOutline.setPosition(5, 5);
-	satisfactionOutline.setSize(sf::Vector2f(200, 30));
-	satisfactionOutline.setOutlineThickness(2);
-	satisfactionOutline.setOutlineColor(sf::Color(255, 0, 0, 50));
-	satisfactionOutline.setFillColor(sf::Color(0, 0, 0, 70));
-	satisfactionFill.setPosition(5, 5);
-	satisfactionFill.setFillColor(sf::Color(255, 0, 0, 30));
+  texture.loadFromFile("res/player.png");
+  sprite.setTexture(texture);
+
+  satisfaction = 0;
+  satisfactionOutline.setPosition(5, 5);
+  satisfactionOutline.setSize(sf::Vector2f(200, 30));
+  satisfactionOutline.setOutlineThickness(2);
+  satisfactionOutline.setOutlineColor(sf::Color(255, 0, 0, 50));
+  satisfactionOutline.setFillColor(sf::Color(0, 0, 0, 70));
+  satisfactionFill.setPosition(5, 5);
+  satisfactionFill.setFillColor(sf::Color(255, 0, 0, 30));
 }
 
 void Player::update() {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-		x -= speed;
-	}
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+    x -= speed;
+  }
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-		x += speed;
-	}
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+    x += speed;
+  }
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-		y -= speed;
-	}
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+    y -= speed;
+  }
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-		y += speed;
-	}
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+    y += speed;
+  }
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::F)) {
-		satisfaction++;
-    
-		if (satisfactionSound.getStatus() != sf::Sound::Playing) {
-			satisfactionSound.play();
-		}
-	} else {
-		satisfactionSound.stop();
-	}
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::F)) {
+    satisfaction++;
 
-	/* Handle satisfaction */
-	satisfactionFill.setSize(sf::Vector2f(satisfaction, 30));
+    if (satisfactionSound.getStatus() != sf::Sound::Playing) {
+      satisfactionSound.play();
+    }
+  } else {
+    satisfactionSound.stop();
+  }
 
-	sprite.setPosition(x, y);
+  /* Handle satisfaction */
+  satisfactionFill.setSize(sf::Vector2f(satisfaction, 30));
+
+  sprite.setPosition(x, y);
 }
 
 bool Player::checkCollision(sf::FloatRect other) {
-	return sprite.getGlobalBounds().intersects(other);
+  return sprite.getGlobalBounds().intersects(other);
 }
